6d57e5245d3628823c1d8703eedf890b48b3db88
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-08-21 07:58:08 -0600

    Improved Jenkins scripts

--- jenkins/ci-1-stop.sh
+++ jenkins/ci-1-stop.sh
@@ -1,39 +1,123 @@
 #!/bin/sh
 #
 
-IMAGE_NAME='com.gft.zuul:latest'
-echo 'Stopping containers based on' $IMAGE_NAME
+#
+# Function/Procedures
+#
 
-echo 'Searching for previously running containers...'
-CONTAINERS_RUNNING=`docker ps -a -q --filter ancestor=$IMAGE_NAME --format="{{.ID}}"`
-
-if test -n "$CONTAINERS_RUNNING";
-then
-
-	echo 'Previously running containers based on the same image are:'
-	echo $CONTAINERS_RUNNING
-
-	echo 'Trying to stop previously running containers...'
-	CONTAINERS_STOPPED=`docker stop $CONTAINERS_RUNNING`
-
-	if test -n "$CONTAINERS_STOPPED";
+cleanUpDocker() {
+	echo 'Looking for dangling images...' 
+	DANGLING_IMAGES=`docker images -f dangling=true -q`
+	
+	if test -n "$DANGLING_IMAGES";
 	then
-		echo "Successfully stopped containers"
 
-		echo "Trying to remove previously running containers"
-		CONTAINERS_REMOVED=`docker rm $CONTAINERS_STOPPED`
+		echo 'Attempting to remove dangling images...'
+		DANGLING_IMAGES_REMOVED=`docker rmi ${DANGLING_IMAGES}`
+
+		if test -n "$DANGLING_IMAGES_REMOVED";
+		then 
+			echo 'Successful'
+		else 
+			echo "ERROR: Couldn't remove dangling images!"
+		fi
+	else
+		echo 'No dangling images were found'
+	fi
+
+	echo 'Looking for dangling volumes...'
+	DANGLING_VOLUMES=`docker volume ls -f dangling=true -q`
+
+	if test -n "$DANGLING_VOLUMES";
+	then
+
+		echo 'Attempting to remove dangling volumes...'
+		DANGLING_VOLUMES_REMOVED=`docker volume rm $DANGLING_VOLUMES`
+
+		if test  -n "$DANGLING_VOLUMES_REMOVED";
+		then 
+			echo 'Successful'
+		else 
+			echo "ERROR: Couldn't remove dangling volumes!"
+		fi
+
+	else 
+		echo 'No dangling volumes were found'
+	fi
+}
+
+removeContainer() {
+	NAME=$1
+	VERSION=$2
+	NAME_VERSION="$NAME:$VERSION"
+
+	echo 'Looking for containers running based on' ${NAME_VERSION} '...'
+	CONTAINERS_RUNNING=`docker ps -a | grep "$NAME_VERSION" | awk '{print $1}'`
+
+	if test -n "$CONTAINERS_RUNNING";
+	then
+
+		echo 'Stopping the containers found...'
+		CONTAINERS_STOPPED=`echo ${CONTAINERS_RUNNING} | xargs docker stop`
 
 		if test -n "$CONTAINERS_STOPPED";
 		then
-			echo "Successfully removed containers"
+
+			echo "Removing the stopped containers..."
+			CONTAINERS_REMOVED=`echo ${CONTAINERS_STOPPED} | xargs docker rm -v`
+
+			if test -n "$CONTAINERS_STOPPED";
+			then
+				echo "Successful"
+			else
+				echo "ERROR: Couldn't remove running containers!"
+			fi
 		else
-			echo "ERROR: Couldn't remove previously running containers!"
+			echo "ERROR: Couldn't stop running containers!"
 		fi
-
 	else
-		echo "ERROR: Couldn't stop previously running containers!"
+		echo 'No containers were found'
 	fi
+}
 
-else
-	echo 'No previously running containers were found'
-fi
+removeImage() {
+	NAME=$1
+	VERSION=$2
+	NAME_VERSION="$NAME:$VERSION"
+
+	echo 'Looking for an image named' ${NAME_VERSION} '...'
+	IMAGE=`docker images | grep -E "$NAME\s+$VERSION" | awk '{print $3}'`
+
+	if test -n "$IMAGE";
+	then
+
+		echo 'Removing the image found...'
+		IMAGE_REMOVED=`echo ${IMAGE} | xargs docker rmi`
+
+		if test -n "$IMAGE_REMOVED";
+		then 
+			echo "Successful"
+		else 
+			echo "ERROR: Couldn't remove the image found"
+		fi
+	else 
+		echo "No image was found"
+	fi
+}
+
+#
+# Actual script
+#
+
+echo 'Cleaning up Docker...'
+cleanUpDocker
+
+# Stop application
+echo 'Removing previous app image and containers...'
+removeContainer 'com.gft.zuul' 'latest' 
+removeImage 'com.gft.zuul' 'latest' 
+
+#
+# References:
+#   - https://www.shellscript.sh/functions.html
+#   - https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
--- jenkins/ci-2-deploy.sh
+++ jenkins/ci-2-deploy.sh
@@ -2,7 +2,14 @@
 #
 
 IMAGE_NAME='com.gft.zuul:latest'
-echo 'Launching new container based on image' $IMAGE_NAME
+echo 'Launching new container based on image' $IMAGE_NAME '...'
 
 docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11002:11002 $IMAGE_NAME
+
+echo 'Waiting 20s for APP container to start...'
+sleep 20s
+
+echo 'APP Logs'
+docker ps -a | grep "$IMAGE_NAME" | awk '{print $1}' | xargs docker logs
+
 echo 'Success'
